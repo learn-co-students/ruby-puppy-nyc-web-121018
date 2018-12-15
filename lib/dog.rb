d5c0297a252ee7725ab7d8a7ed1cc25c7414935b
sharicodes <srosenb@yahoo.com>
2018-12-15 15:37:26 -0500

    finished

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Dog
+attr_reader :name
+
+@@all = []
+
+
+def initialize(name)
+  @name = name
+  @@all << self
+
+end
+
+def self.all
+@@all.each {|dog| puts dog.name}
+end
+
+
+
+
+def self.clear_all
+  @@all.clear
+  @@all
+
+end
+
+
+
+
+
+end
